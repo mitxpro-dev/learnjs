bb16f75eeaa83c2f131429d9d917cbcda434d8d5
Peter Chu <peterc0523@gmail.com>
2023-06-25 13:28:56 -0400

    feat/issue-8/add_nav_and_footer

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+import { Text, VStack } from '@chakra-ui/react'
+
+export default function About() {
+  return (
+    <VStack justify='center' h='100vh'>
+      <Text>About page</Text>
+    </VStack>
+  )
+}
---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react'
+import { Text, Flex } from '@chakra-ui/react'
+
+export const Footer = () => {
+  const currentYear = new Date().getFullYear();
+  return (
+    <Flex justify='center'>
+      <Text fontSize='xs'>Copyright &copy; {currentYear} MIT xPRO Development Group </Text>
+    </Flex>
+  )
+}

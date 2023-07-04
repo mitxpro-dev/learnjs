5ae3811a392faed0e213f10f16924dffc3f55a26
Peter Chu <peterc0523@gmail.com>
2023-07-04 09:33:00 -0400

    refactor: map through about content

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,23 +1,53 @@
 'use client'
 import { Box, Heading, Text, VStack } from '@chakra-ui/react'
 
+interface AboutSection {
+  title: string
+  text: string | string[]
+}
+
+const aboutContent: AboutSection[] = [
+  {
+    title: 'The Project',
+    text: [
+      `This application, using NextJS, provides users the ability to complete coding challenges and learn more about specific, common problems that arise in software development.`,
+      `The main goal of this project is to assist new developers in preparing for common challenges they will encounter when entering the software development industry and building production-level software.`,
+    ],
+  },
+  {
+    title: 'The Team',
+    text: `We are full stack developers who graduated from the MIT xPRO Professional Certificate in Coding program.`,
+  },
+  {
+    title: 'How to Contribute',
+    text: `This project is for alumni of the MIT xPRO Professional Certification in Coding program. Reach out to @Pete or @Spencer in the "MIT xPRO Coding" Slack channel.`,
+  },
+]
+
 export default function About() {
   return (
     <VStack px='4' py='16' gap='6'>
-      <Heading display='flex' justifyContent='center'>About Learn JS</Heading>
-      <Box display='flex' flexDir='column' gap='2' w='100%'>
-        <Heading as="h3" size='l'>The Project</Heading>
-        <Text>This application, using NextJS, provides users the ability to complete coding challenges and learn more about specific, common problems that arise in software development.</Text>
-        <Text>The main goal of this project is to assist new developers in preparing for common challenges they will encounter when entering the software development industry and building production-level software.</Text>
-      </Box>
-      <Box display='flex' flexDir='column' gap='2' w='100%'>
-        <Heading as="h3" size='l'>The Team</Heading>
-        <Text>We are full stack developers who graduated from the MIT xPRO Professional Certificate in Coding program.</Text>
-      </Box>
-      <Box display='flex' flexDir='column' gap='2' w='100%'>
-        <Heading as="h3" size='l'>How to Contribute</Heading>
-        <Text>This project is for alumni of the MIT xPRO Professional Certification in Coding program. Reach out to @Pete or @Spencer in the &quot;MIT xPRO Coding&quot; Slack channel.</Text>
-      </Box>
+      <Heading display='flex' justifyContent='center'>
+        About Learn JS
+      </Heading>
+      {aboutContent.map(({ title, text }) => (
+        <Box
+          key={`about-section-${title}`}
+          display='flex'
+          flexDir='column'
+          gap='2'
+          w='full'
+        >
+          <Heading as='h3' size='lg'>
+            {title}
+          </Heading>
+          {Array.isArray(text) ? (
+            text.map((textElement, i) => <Text key={`about-subsection-${title}#${i}`}>{textElement}</Text>)
+          ) : (
+            <Text>{text}</Text>
+          )}
+        </Box>
+      ))}
     </VStack>
   )
 }

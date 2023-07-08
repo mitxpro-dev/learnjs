38f9e294b58f93241db06fb38b963207d571c038
HudsonGraeme <me@hudsongrae.me>
2023-07-08 00:46:12 -0400

    feat: Add react-markdown implementation

--- src/pages/challenges/challenge.tsx
+++ src/pages/challenges/challenge.tsx
@@ -1,12 +1,16 @@
 import { Text, VStack } from '@chakra-ui/react'
 import { useParams } from 'react-router-dom'
+import ChallengeBody from '../../components/ChallengeBody'
 
 export default function SingleChallenge() {
   const { id } = useParams()
 
   return (
-    <VStack justify='center' h='100vh'>
-      <Text>Single challenge: {id}</Text>
+    <VStack justify='center' minH='100vh' py={8}>
+      <VStack w='full' h='full' bg={'blackAlpha.100'} rounded='lg' p={16}>
+        <Text>Single challenge: {id}</Text>
+        <ChallengeBody />
+      </VStack>
     </VStack>
   )
 }

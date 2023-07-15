6e46dbe2b6b62b18dccc17f58361c7b667d7736a
HudsonGraeme <me@hudsongrae.me>
2023-07-15 00:18:47 -0400

    bug: fix blank page and base URL in prod

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  base: '/',
+  base: process.env.NODE_ENV === 'production' ? '/learnjs/' : '/',
 })
